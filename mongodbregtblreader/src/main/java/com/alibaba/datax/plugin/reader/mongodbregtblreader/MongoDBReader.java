52d451bd19ef535d8784a7b71008fd2ac813fc66
stevan <734506700qq@gmail.com>
2020-12-17 22:57:06 +0800

    #添加正则表名插件#

--- mongodbregtblreader/src/main/java/com/alibaba/datax/plugin/reader/mongodbregtblreader/MongoDBReader.java
+++ mongodbregtblreader/src/main/java/com/alibaba/datax/plugin/reader/mongodbregtblreader/MongoDBReader.java
@@ -1,4 +1,4 @@
-package com.alibaba.datax.plugin.reader.mongodbreader;
+package com.alibaba.datax.plugin.reader.mongodbregtblreader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,8 +17,8 @@ import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.plugin.RecordSender;
 import com.alibaba.datax.common.spi.Reader;
 import com.alibaba.datax.common.util.Configuration;
-import com.alibaba.datax.plugin.reader.mongodbreader.util.CollectionSplitUtil;
-import com.alibaba.datax.plugin.reader.mongodbreader.util.MongoUtil;
+import com.alibaba.datax.plugin.reader.mongodbregtblreader.util.CollectionSplitUtil;
+import com.alibaba.datax.plugin.reader.mongodbregtblreader.util.MongoUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
