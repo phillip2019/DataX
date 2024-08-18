e6137c72a9c886d74f804cfa6bb293d7e193b453
phillip2019 <msn734506700@live.com>
2024-08-18 23:01:01 +0800

    #Updates#redis写入插件升级，先删除老key数据，再写入数据

--- rediswriter/src/main/java/com/alibaba/datax/plugin/writer/rediswriter/RedisWriteAbstract.java
+++ rediswriter/src/main/java/com/alibaba/datax/plugin/writer/rediswriter/RedisWriteAbstract.java
@@ -145,7 +145,7 @@ public abstract class RedisWriteAbstract {
                 records++;
             }
             this.syncData();
-            logger.info("Delete old data, total size: {}, process percentage: {}", keyResultList.size(), (i + batchSize) * 100.0 / keyResultList.size());
+            logger.info("Delete old data, total size: {}, process percentage: {}%", keyResultList.size(), (i + batchSize) * 100.0 / keyResultList.size());
         }
         this.syncAllData();
         logger.info("End delete old data, keyPrefix: {}, deleted size: {}", keyPrefix, keyResultList.size());
